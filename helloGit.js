0d49ac313aa45ddd6f75c3699f6426f907109072
BlackDev <rojas.ariel.397@gmail.com>
2023-10-16 20:59:38 -0300

    Modificamos el local sin modificar el remoto nuevamente :D

--- helloGit.js
+++ helloGit.js
@@ -2,3 +2,4 @@ console.log('HellO Git! with new changes')
 console.log('Que tal curso?')
 console.log("Haremos un cambio para el repositorio local pero sin modificar al remoto respecto al remoto");
 console.log("Actualizamos el repositorio remoto");
+console.log("Hacemos un nuevo cambio y verificamos nuevamente! :D");
