1d47be9913ba50862f7d483b2c4b5dd7e75ae5b5
BlackDev <rojas.ariel.397@gmail.com>
2023-10-17 18:40:47 -0300

    agregamos un cambio desde el repositorio clonado

--- helloGit.js
+++ helloGit.js
@@ -3,3 +3,5 @@ console.log('Que tal curso?')
 console.log("Haremos un cambio para el repositorio local pero sin modificar al remoto respecto al remoto");
 console.log("Actualizamos el repositorio remoto");
 console.log("Hacemos un nuevo cambio y verificamos nuevamente! :D");
+console.log("hacemos una modificacion desde el repositorio clonado!")
+
